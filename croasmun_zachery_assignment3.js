5e6db96c40250397dc6a65d1b3176a2ae6dbbe3d
Zachery croasmun <th3p4ler1der@gmail.com>
2013-02-19 19:05:45 -0800

    json addition

--- croasmun_zachery_assignment3.js
+++ croasmun_zachery_assignment3.js
@@ -4,3 +4,15 @@ SDI 1302 Project 3
 */
 alert("JavaScript works!");
 
+
+var wizard = {
+	"name":"sam",
+	"spellList":[" freeze "," fire "," death "],
+	"pack":[],	
+	
+	
+	
+	
+};
+
+console.log(wizard.spellList[2]);
